e820b1b886fe8f90f8cf4e3dc151113df3e5cac4
TinoMuzambi <tinomuzambi@gmail.com>
2024-03-18 08:14:06 +0200

    Adds dense rank.

--- MZMTIN002.R
+++ MZMTIN002.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 
 dat <- list.files("data/", "*.csv", full.names = T) %>% 
-  read_csv(., id = "run")
+  read_csv(., id = "run") %>% 
+  mutate(run = dense_rank(run))
 
 head(dat)
