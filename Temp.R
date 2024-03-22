709adb8e26f1dd86365fa0f09482fbb6de2beff5
TinoMuzambi <tinomuzambi@gmail.com>
2024-03-22 14:51:22 +0200

    Computes total distance.

--- Temp.R
+++ Temp.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(leaflet)
+library(geosphere)
 dat <- list.files("data/", "*.csv", full.names = T) %>% 
   read_csv(., id = "run") %>% 
   mutate(run = dense_rank(run))
@@ -18,4 +19,17 @@ leaflet() %>%
   setView(lng = mean(lng), lat = mean(lat), zoom = 12) %>%
   addTiles() %>% # Add base map tiles
   addPolylines(lng = lng, lat = lat, data = run_data_filtered, color = "blue") # Plot run path
+
+# Compute distance between consecutive points
+dat <- dat %>% 
+  mutate(distance = c(0, distHaversine(
+    cbind(dat$lng[-nrow(dat)], dat$lat[-nrow(dat)]),
+    cbind(dat$lng[-1], dat$lat[-1])
+  ))) %>% 
+  mutate(distance = distance / 1000)
+
+# Group by run and sum distances to get total distance for each run
+run_distances <- dat %>%
+  group_by(run) %>%
+  summarize(total_distance = sum(distance, na.rm = TRUE))
   
